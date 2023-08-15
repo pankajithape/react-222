1567e11c9d59ef0a19accb315374fe57d93e81cb
Pankaj Ithape <pankajithape95@gmail.com>
2023-08-15 19:34:10 +0530

    removed ui toggle redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import uiSlice from './ui-slice';
 import cartSlice from './cart-slice';
 import searchSlice from './search-slice';
 
 const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer, cart: cartSlice.reducer, search: searchSlice.reducer
+    cart: cartSlice.reducer, search: searchSlice.reducer
   },
 });
 
